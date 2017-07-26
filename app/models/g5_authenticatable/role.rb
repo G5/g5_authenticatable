f654524008b30945b42bcc7bcec32ca9966318be
Levi Brown <levi.brown@getg5.com>
2017-07-26 09:38:45 -0700

    fix for persisting roles

--- app/models/g5_authenticatable/role.rb
+++ app/models/g5_authenticatable/role.rb
@@ -4,7 +4,7 @@ module G5Authenticatable
   # A user role (e.g. admin, viewer), optionally scoped to a client or location
   class Role < ActiveRecord::Base
     has_and_belongs_to_many :users, join_table: :g5_authenticatable_users_roles
-    belongs_to :resource, polymorphic: true
+    belongs_to :resource, polymorphic: true, optional: true
 
     scopify
   end
