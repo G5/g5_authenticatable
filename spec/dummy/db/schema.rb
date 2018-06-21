277b1545d52bf6c5524cf7b6d3f0a149f272d168
maeve <maeve.revels@getg5.com>
2018-06-20 19:28:07 -0700

    Update dummy schema

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20170613201436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "g5_authenticatable_roles", id: :serial, force: :cascade do |t|
     t.string "name"
