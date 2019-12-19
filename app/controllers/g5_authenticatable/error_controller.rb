53f21b1b565f077f0a1f4846320ccfa256f31631
Nathan Struhs <nathan.struhs@getg5.com>
2019-12-19 10:41:12 -0800

    Include css & js files, style error view

--- app/controllers/g5_authenticatable/error_controller.rb
+++ app/controllers/g5_authenticatable/error_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 module G5Authenticatable
-  # For handling errors returned by the auth server
   class ErrorController < G5Authenticatable::ApplicationController
     def auth_error
       flash[:error] = 'There was a problem with the Auth Server!'
